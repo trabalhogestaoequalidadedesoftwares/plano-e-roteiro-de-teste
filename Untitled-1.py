62efced0f8a8b5ca6ad265df60cbaac2d6169815
VictorGa2003 <115746295+VictorGa2003@users.noreply.github.com>
2023-10-23 19:19:06 -0300

    testes bedroom dreams
    testes do codigo do sistema

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+def test_pesquisa_sem_resultados():
+    nome_hotel = "trivago"
+    base_de_dados_hoteis = ["Hotel A", "Hotel B", "Hotel C"]
+    if nome_hotel in base_de_dados_hoteis:
+        print(f"O hotel '{nome_hotel}' foi encontrado na busca.")
+    else:
+        print(f"Desculpe, não encontramos resultados para o hotel '{nome_hotel}'.")
+
+test_pesquisa_sem_resultados()
